bb1f6aa0348b99be618170c55ad0096ab5cdb98b
BountyHunterOka <434291091spd@gmail.com>
2025-09-07 20:25:26 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -135,7 +135,7 @@ def extract_order_id(decrypted_json_str):
             if voice_talking and any(keyword in name for keyword in ['连麦','听歌'] for name in names):
                 log("[跳过订单] 不要连麦单")
                 continue
-             service_price = order.get("servicePrice", 0)
+            service_price = order.get("servicePrice", 0)
             if service_price <= 1500:
                 log(f"[跳过订单] 价格过低 (servicePrice={service_price})")
                 continue
