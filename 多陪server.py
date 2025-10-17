efaf6532cad96d1420eeedec6c512c0eb4be5fab
BountyHunterOka <434291091spd@gmail.com>
2025-10-17 20:14:16 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -167,7 +167,7 @@ def confirm_order(order_id):
     data = {"id": order_id}
     try:
         while running:
-            time.sleep(7)
+            time.sleep(6.5)
             resp = session.post(url, data=data, timeout=3.5)
             da = resp.json()
             confirm_rep = decrypt_aes_cbc(da["response"], KEY_HEX, IV_HEX)
